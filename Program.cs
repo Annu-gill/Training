3c01b0119681c27cfcc8c022f7648841c1d5a215
Annu Gill <annugill005@gmail.com>
2025-12-18 23:38:27 +0530

    1st commit

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        // code to write and read your name in C#
+        // Console.WriteLine("Enter your name:");
+        // string? name= Console.ReadLine();
+        // Console.WriteLine("Hello, "+name+"!");
+
+
+        // using if-else statements in C#
+        // int age=20;
+        // if (age >= 18)
+        // {
+        //     Console.WriteLine("Eligible");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Not Eligible");
+        // }
+
+        // check whether the person is adult or not
+        // Console.WriteLine("Enter age: ");
+        // String? input=Console.ReadLine();
+        // if (int.TryParse(input, out int age))
+        // {
+        //     bool isAdult=age>=18;
+        //     Console.WriteLine("Adult? "+isAdult);
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Invalid age. please enter a whole number.");
+        // }
+
+
+        // CODE to convert feet to centimeters
+
+        const double CmPerFoot = 30.48;
+
+        Console.Write("Enter feet: ");
+        string? input = Console.ReadLine();
+
+        if (!double.TryParse(input, out double feet))
+        {
+            Console.WriteLine("Invalid input. Please enter a number.");
+            return;
+        }
+
+        if (feet < 0)
+        {
+            Console.WriteLine("Feet cannot be negative.");
+            return;
+        }
+
+        double cm = feet * CmPerFoot;
+        Console.WriteLine($"{feet} feet = {cm:F2} cm");
+    }
+}
