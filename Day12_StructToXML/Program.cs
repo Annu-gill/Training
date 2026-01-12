663e7901f6cc4c95ce2deae82d2e247228dc1dd0
Annu Gill <annugill005@gmail.com>
2026-01-12 14:45:02 +0530

    Adding files.

--- Day12_StructToXML/Program.cs
+++ Day12_StructToXML/Program.cs
@@ -69,48 +69,100 @@
 // }
 
 
-// write code with a student object and properties as id, name, marks and marks need to be stored in list instead of array
-// Convert marks to list instead of array
+// // write code with a student object and properties as id, name, marks and marks need to be stored in list instead of array
+// // Convert marks to list instead of array
+
+// using System;
+// using System.IO;
+// using System.Collections.Generic;
+// using System.Xml.Serialization;
+
+// public class Student
+// {
+//     public int Id { get; set; }
+//     public string Name { get; set; }
+//     // Marks stored in array
+//     // public int[] Marks { get; set; }
+//     // Marks stored in List instead of array
+//     public List<int> Marks { get; set; }
+
+//     // Parameterless constructor (required)
+//     public Student()
+//     {
+//         // Marks = new int[0];  // array
+//         Marks = new List<int>();  // for list
+//     }
+// }
+// class Program
+// {
+//     static void Main()
+//     {
+//         Student student = new Student
+//         {
+//             Id = 1,
+//             Name = "Annu",
+//             // Marks = new int[] { 85, 90, 78, 92 }  // for array
+//             Marks = new List<int> { 85, 90, 78, 92 }  // for list
+//         };
+
+//         XmlSerializer serializer = new XmlSerializer(typeof(Student));
+
+//         using (StringWriter writer = new StringWriter())
+//         {
+//             serializer.Serialize(writer, student);
+//             Console.WriteLine(writer.ToString());
+//         }
+//     }
+// }
+
+
+// // dictionary inside object and when u try to call it, it's not.
+
+// Q.
+// take object data and serialize it in json
+// take json data deserialize it
 
 using System;
-using System.IO;
-using System.Collections.Generic;
-using System.Xml.Serialization;
+// using System.Collections.Generic;
+// using System.Text.Json;
 
-public class Student
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    // Marks stored in array
-    // public int[] Marks { get; set; }
-    // Marks stored in List instead of array
-    public List<int> Marks { get; set; }
+// public class Student
+// {
+//     public int Id { get; set; }
+//     public string? Name { get; set; }
+//     public List<int>? Marks { get; set; }
+// }
+// class Program
+// {
+//     static void Main()
+//     {
+//         // OBJECT → JSON (Serialization)
+//         Student student = new Student
+//         {
+//             Id = 1,
+//             Name = "Annu",
+//             Marks = new List<int> { 85, 90, 88 }
+//         };
 
-    // Parameterless constructor (required)
-    public Student()
-    {
-        // Marks = new int[0];  // array
-        Marks = new List<int>();  // for list
-    }
-}
-class Program
-{
-    static void Main()
-    {
-        Student student = new Student
-        {
-            Id = 1,
-            Name = "Annu",
-            // Marks = new int[] { 85, 90, 78, 92 }  // for array
-            Marks = new List<int> { 85, 90, 78, 92 }  // for list
-        };
+//         string jsonData = JsonSerializer.Serialize(student);
+//         Console.WriteLine("Serialized JSON:");
+//         Console.WriteLine(jsonData);
 
-        XmlSerializer serializer = new XmlSerializer(typeof(Student));
+//         // JSON → OBJECT (Deserialization)
+//         Student deserializedStudent =
+//             JsonSerializer.Deserialize<Student>(jsonData);
 
-        using (StringWriter writer = new StringWriter())
-        {
-            serializer.Serialize(writer, student);
-            Console.WriteLine(writer.ToString());
-        }
-    }
-}
+//         Console.WriteLine("\nDeserialized Object:");
+//         Console.WriteLine($"Id: {deserializedStudent.Id}");
+//         Console.WriteLine($"Name: {deserializedStudent.Name}");
+//         Console.WriteLine("Marks:");
+//         foreach (var mark in deserializedStudent.Marks)
+//         {
+//             Console.WriteLine(mark);
+//         }
+//     }
+// }
+
+
+
+
---
+++ Practice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Practice
+{
+    public class Employee
+    {
+        public int Id{get; set;}
+        public string Name{get;set;}
+        public int Salary{get;set;}
+        public void Display()
+        {
+            Console.WriteLine($"Id: "+Id+"Name: "+Name+"Salary: ");
+
+        }
+        
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Employee emp=new Employee(1, "Annu",100000);
+        }
+    }
+}
